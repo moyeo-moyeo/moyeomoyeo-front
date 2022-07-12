d4491e1439cd9fdc9c98dda7133d444d53b7de9d
junsu <motors3996@gmail.com>
2022-07-12 20:39:50 +0900

    add: header,nav,article,footer를 포함한 메인페이지

---
+++ front/src/components/Nav.js
@@ -0,0 +1,8 @@
+export default function Nav() {
+
+  return <nav className="nav">
+    <a href='/' className="map-btn">너와 나의 중간지점 찾기</a>
+    <a href='/' className="calculator">친구랑 N빵 계산하기</a>
+    <a href='/' className="history">과거사용내역</a>
+  </nav>
+}
