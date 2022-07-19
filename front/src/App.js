ff9a37ab47581e584b4ea518c73cad72e83649db
3pointdev <motors3996@gmail.com>
2022-07-19 23:08:25 +0900

    first Commit

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,56 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Join from "./pages/Join";
-import Login from "./pages/Login";
+import React,{useState} from 'react';
 // import axios from 'axios';
-
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Map from './components/Map';
+import Footer from './components/Footer';
+import Division from './components/Division';
+import Login from './components/Login'
+import Search from './components/Search'
+import Join from './components/Join'
 
 function App() {
+
+  let article = null;
+  const [mode, setMode] = useState("map-btn");
+
+  if(mode === "map-btn" || mode === "logout"){
+    article = <Map/>
+  }else if(mode === "calculator"){
+    article = <Division/>
+  }else if(mode === "login"){
+    article = <Login onChange={(id) =>{
+      setMode(id);
+    }}/>
+  }else if(mode === "join"){
+    article = <Join/>
+  }else if(mode === "search_id" || mode === "search_go_id"){
+    article = <><Login/><Search searchId={mode} onChange={(id)=>{
+      setMode(id);
+    }}/></>
+  }else if(mode === "search_pw" || mode === "search_go_pw"){
+    article = <><Login/><Search searchId={mode} onChange={(id)=>{
+      setMode(id);
+    }}/></>
+  }else if(mode === "myPage"){
+    article = <>test</>
+  }else if(mode === "myPage_history"){
+    article = <>test</>
+  }
+
+  console.log(mode);
   return (
-      <BrowserRouter basename="">
-        <Routes>     
-          <Route path = "/" element = {<Home />}/>
-          <Route path = "/Join" element = {<Join />}/>
-          <Route path = "/Login" element = {<Login />}/>
-        </Routes>
-      </BrowserRouter>
+    <div className="App">
+      <Header onChange={(className)=>{
+        setMode(className);
+      }}/>
+      <Nav onChange={(className)=>{
+        setMode(className);
+      }}/>
+      {article}
+      <Footer />
+    </div>
   );
 }
 
--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,10 +1,30 @@
+import React,{useState} from 'react';
 import Logo from '../data/Logo.png'
 
-export default function Header() {
+export default function Header(props) {
+  let login = null;
+  const [loginState,setLoginState] = useState("logined");
+
+  if(loginState === "unlogin"){
+    login = <a href='/' className="login" onClick={(event)=>{
+    event.preventDefault();
+    props.onChange(event.target.className);
+  }}>로그인</a>
+  } else if(loginState === "logined"){
+    login = <><a href='/' id='myPage' className="login" onClick={(event)=>{
+      event.preventDefault();
+      props.onChange(event.target.id);
+    }}>마이페이지</a>
+    <a href='/' id='logout' className="login" onClick={(event)=>{
+      event.preventDefault();
+      setLoginState("unlogin");
+      props.onChange(event.target.id);
+    }}>로그아웃</a></>
+  }
 
   return <header className="header">
     <a href='/' className="brand-name">모여모여</a>
     <a href='/'><img src={Logo} className="logo" alt='logo'/></a>
-    <a href='/' className="login-btn">로그인</a>
+    {login}
   </header>
 }
