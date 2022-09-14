56bc11384e40b7bf8a6e5ba5c01f57684e2ff922
sejung1997 <ksgve1997814@naver.com>
2022-09-14 23:31:48 +0900

    api확인

--- front/src/pages/Join.jsx
+++ front/src/pages/Join.jsx
@@ -1,223 +1,218 @@
-import '../css/join.css';
-import {useEffect, useState} from "react";
+import "../css/join.css";
+import { useEffect, useState } from "react";
+import { ApiRq } from "../utils/apiConfig";
+import axios from "axios";
 
 export default function Join() {
-    /**
-     * 회원가입 요청 시 필요 데이터
-     */
-    const [requestBody,setRequestBody] = useState(
-        {
-            loginId:'',
-            password:'',
-            confirmPassword:'',
-            username:'',
-            nickname:'',
-            birthDate:'',
-            email:'', //차후 수정
-            emailAddress:'direct input', //기본값 : 직접입력
-            emailCertification:'' //이메일 인증번호
-        }
-    )
-    /**
-     * 비밀번호, 비밀번호 확인 체크 값 -> 같으면 true, 다르면 false
-     */
-    const [passwordCheck,setPasswordCheck] = useState(false)
-    /**
-     * 회원가입 데이터 바인딩
-     * @param e
-     */
-    const handleValue = e => {
-        const { name, value } = e.target;
-        setRequestBody(previousState => {
-            return { ...previousState, [name]: value };
-        });
-    };
-    /**
-     * 비밀번호, 비밀번호 확인 같은지 체크
-     */
-    useEffect(()=>{
-        setPasswordCheck(requestBody.password !== ''&& requestBody.password === requestBody.confirmPassword)
-    },[requestBody.password,requestBody.confirmPassword])
+  /**
+   * 회원가입 요청 시 필요 데이터
+   */
+  const [requestBody, setRequestBody] = useState({
+    loginId: "",
+    password: "",
+    confirmPassword: "",
+    username: "",
+    nickname: "",
+    birthDate: "",
+    email: "", //차후 수정
+    emailAddress: "direct input", //기본값 : 직접입력
+    emailCertification: "", //이메일 인증번호
+  });
+  /**
+   * 비밀번호, 비밀번호 확인 체크 값 -> 같으면 true, 다르면 false
+   */
+  const [passwordCheck, setPasswordCheck] = useState(false);
+  /**
+   * 회원가입 데이터 바인딩
+   * @param e
+   */
+  const handleValue = (e) => {
+    const { name, value } = e.target;
+    setRequestBody((previousState) => {
+      return { ...previousState, [name]: value };
+    });
+  };
+  /**
+   * 비밀번호, 비밀번호 확인 같은지 체크
+   */
+  useEffect(() => {
+    setPasswordCheck(
+      requestBody.password !== "" &&
+        requestBody.password === requestBody.confirmPassword
+    );
+  }, [requestBody.password, requestBody.confirmPassword]);
 
-    /**
-     * 비밀번호 예외 문구
-     */
-    const passwordErrorText = () => {
-        let text = ''
-        if(requestBody.password ==='') text='비밀번호를 입력해주세요'
-        else text=''
-        return text
+  /**
+   * 비밀번호 예외 문구
+   */
+  const passwordErrorText = () => {
+    let text = "";
+    if (requestBody.password === "") text = "비밀번호를 입력해주세요";
+    else text = "";
+    return text;
+  };
+  /**
+   * 비밀번호 확인 예외 문구
+   */
+  const passwordConfirmErrorText = () => {
+    let text = "";
+    if (requestBody.confirmPassword === "") text = "비밀번호를 입력해주세요";
+    else if (!passwordCheck) text = "비밀번호를 확인해주세요";
+    else text = "";
+    return text;
+  };
+
+  /**
+   * 아이디 중복검사 요청
+   */
+  const getDuplicateIdRq = () => {
+    // Get /loginid/duplicate?loginid="test"
+    if (true) {
+      //   axios.get('/loginid/duplicate?loginid="test"');
+      ApiRq("GET", '/loginid/duplicate?loginid="test"');
+      alert("아이디 중복검사 요청 중");
     }
-    /**
-     * 비밀번호 확인 예외 문구
-     */
-    const passwordConfirmErrorText = () => {
-        let text = ''
-        if(requestBody.confirmPassword ==='') text='비밀번호를 입력해주세요'
-        else if(!passwordCheck) text= "비밀번호를 확인해주세요"
-        else text=''
-        return text
+  };
+  /**
+   * 닉네임 중복검사 요청
+   */
+  const getDuplicateNicknameRq = () => {
+    if (true) {
+      alert("닉네임 중복검사 요청 중");
     }
+  };
 
-    /**
-     * 아이디 중복검사 요청
-     */
-    const getDuplicateIdRq = () => {
-        if(true){
-            alert("아이디 중복검사 요청 중")
-        }
-
+  const postJoinRq = () => {
+    if (true) {
+      alert("로그인 요청 중");
     }
-    /**
-     * 닉네임 중복검사 요청
-     */
-    const getDuplicateNicknameRq = () => {
-        if(true){
-            alert("닉네임 중복검사 요청 중")
-        }
-    }
+  };
 
-    const postJoinRq = () => {
-        if(true){
-            alert("로그인 요청 중")
-        }
-    }
-
-
-
-    return (
-        <div className="join-wrapper">
-            <header className="join-header">
-                <p className="join-title">모여모여</p>
-            </header>
-            <main className="join-contents">
-                <div>
-                    <p>아이디 : </p>
-                    <div>
-                        <input
-                            className="join-input-with-btn"
-                            value={requestBody.loginId}
-                            placeholder="아이디를 입력하세요"
-                            type="text"
-                            name="loginId"
-                            onChange={handleValue}
-                        />
-                        <button className="join-duple-btn" onClick={getDuplicateIdRq}>
-                            중복확인
-                        </button>
-                    </div>
-                </div>
-                <div>
-                    <p>비밀번호 : </p>
-                    <input
-                        className="join-input"
-                        value={requestBody.password}
-                        placeholder="비밀번호를 입력하세요"
-                        type="password"
-                        name="password"
-                        onChange={handleValue}
-                    />
-                    <div className="join-error">{passwordErrorText()}</div>
-                </div>
-                <div>
-                    <p>비밀번호 재확인 :</p>
-                    <input
-                        className="join-input"
-                        value={requestBody.confirmPassword}
-                        placeholder="비밀번호를 입력하세요"
-                        type="password"
-                        name="confirmPassword"
-                        onChange={handleValue}
-                    />
-                    <div className="join-error">{passwordConfirmErrorText()}</div>
-                </div>
-                <div>
-                    <p>이름 : </p>
-                    <input
-                        className="join-input"
-                        value={requestBody.username}
-                        placeholder="사용자의 이름을 입력하세요"
-                        type="text"
-                        name="username"
-                        onChange={handleValue}
-                    />
-
-                </div>
-                <div>
-                    <p>닉네임 : </p>
-                    <div>
-                        <input
-                            className="join-input-with-btn"
-                            value={requestBody.nickname}
-                            placeholder="닉네임을 입력하세요"
-                            type="text"
-                            name="nickname"
-                            onChange={handleValue}
-                        />
-                        <button className="join-duple-btn" onClick={getDuplicateNicknameRq}>
-                            중복확인
-                        </button>
-                    </div>
-                </div>
-                <div>
-                    <p>생년월일 : </p>
-                    <input
-                        className="join-input"
-                        value={requestBody.birthDate}
-                        placeholder="사용자의 생년월일을 입력하세요"
-                        type="text"
-                        name="birthDate"
-                        onChange={handleValue}
-                    />
-                </div>
-                <div>
-                    <p>이메일 : </p>
-
-                    <div className="join-phone-div">
-                        <input
-                            className="join-input-with-btn"
-                            placeholder="이메일을 입력하세요"
-                            type="text"
-                            name="email"
-                            onChange={handleValue}
-                            value={requestBody.email}
-                        />
-                        <select
-                            className="telecom"
-                            name="emailAddress"
-                            value={requestBody.emailAddress}
-                            onChange={handleValue}
-                        >
-                            <option value="naver.com">
-                                네이버
-                            </option>
-                            <option value="daum.net">
-                                다음
-                            </option>
-                            <option value="google.com">
-                                구글
-                            </option>
-                            <option value="direct input">
-                                직접입력
-                            </option>
-                        </select>
-                    </div>
-                    <div>
-                        <input className="join-input"
-                               placeholder="인증번호를 입력하세요"
-                               name="emailCertification"
-                               value={requestBody.emailCertification}
-                               onChange={handleValue} />
-                    </div>
-                    <button className="join-duple-btn">인증받기</button>
-                </div>
-                <div>
-                    <button className="join-btn" onClick={postJoinRq}>
-                        가입하기
-                    </button>
-                </div>
-            </main>
+  return (
+    <div className="join-wrapper">
+      <header className="join-header">
+        <p className="join-title">모여모여</p>
+      </header>
+      <main className="join-contents">
+        <div>
+          <p>아이디 : </p>
+          <div>
+            <input
+              className="join-input-with-btn"
+              value={requestBody.loginId}
+              placeholder="아이디를 입력하세요"
+              type="text"
+              name="loginId"
+              onChange={handleValue}
+            />
+            <button className="join-duple-btn" onClick={getDuplicateIdRq}>
+              중복확인
+            </button>
+          </div>
         </div>
+        <div>
+          <p>비밀번호 : </p>
+          <input
+            className="join-input"
+            value={requestBody.password}
+            placeholder="비밀번호를 입력하세요"
+            type="password"
+            name="password"
+            onChange={handleValue}
+          />
+          <div className="join-error">{passwordErrorText()}</div>
+        </div>
+        <div>
+          <p>비밀번호 재확인 :</p>
+          <input
+            className="join-input"
+            value={requestBody.confirmPassword}
+            placeholder="비밀번호를 입력하세요"
+            type="password"
+            name="confirmPassword"
+            onChange={handleValue}
+          />
+          <div className="join-error">{passwordConfirmErrorText()}</div>
+        </div>
+        <div>
+          <p>이름 : </p>
+          <input
+            className="join-input"
+            value={requestBody.username}
+            placeholder="사용자의 이름을 입력하세요"
+            type="text"
+            name="username"
+            onChange={handleValue}
+          />
+        </div>
+        <div>
+          <p>닉네임 : </p>
+          <div>
+            <input
+              className="join-input-with-btn"
+              value={requestBody.nickname}
+              placeholder="닉네임을 입력하세요"
+              type="text"
+              name="nickname"
+              onChange={handleValue}
+            />
+            <button className="join-duple-btn" onClick={getDuplicateNicknameRq}>
+              중복확인
+            </button>
+          </div>
+        </div>
+        <div>
+          <p>생년월일 : </p>
+          <input
+            className="join-input"
+            value={requestBody.birthDate}
+            placeholder="사용자의 생년월일을 입력하세요"
+            type="text"
+            name="birthDate"
+            onChange={handleValue}
+          />
+        </div>
+        <div>
+          <p>이메일 : </p>
 
-    )
+          <div className="join-phone-div">
+            <input
+              className="join-input-with-btn"
+              placeholder="이메일을 입력하세요"
+              type="text"
+              name="email"
+              onChange={handleValue}
+              value={requestBody.email}
+            />
+            <select
+              className="telecom"
+              name="emailAddress"
+              value={requestBody.emailAddress}
+              onChange={handleValue}
+            >
+              <option value="naver.com">네이버</option>
+              <option value="daum.net">다음</option>
+              <option value="google.com">구글</option>
+              <option value="direct input">직접입력</option>
+            </select>
+          </div>
+          <div>
+            <input
+              className="join-input"
+              placeholder="인증번호를 입력하세요"
+              name="emailCertification"
+              value={requestBody.emailCertification}
+              onChange={handleValue}
+            />
+          </div>
+          <button className="join-duple-btn">인증받기</button>
+        </div>
+        <div>
+          <button className="join-btn" onClick={postJoinRq}>
+            가입하기
+          </button>
+        </div>
+      </main>
+    </div>
+  );
 }
--- front/src/pages/Map.jsx
+++ front/src/pages/Map.jsx
@@ -1,69 +1,96 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import KakaoMapCategory from "../components/KakaoMapCategory";
-import '../css/map.css';
+import "../css/map.css";
 
 function SelectList() {
-
-  const [moyeoList,setMoyeoList] = useState([ //임시데이터
-    {key : 1, title : "회운님과 약속", peoples : "(2)", value : ["장준수","정회운"]},
-    {key : 2, title : "지수님과 약속", peoples : "(2)", value : ["장준수","성지수"]},
-    {key : 3, title : "프로젝트 약속", peoples : "(5)", value : ["장준수","정회운","성지수","최강현","황수경"]}
+  const [moyeoList, setMoyeoList] = useState([
+    //임시데이터
+    {
+      key: 1,
+      title: "회운님과 약속",
+      peoples: "(2)",
+      value: ["장준수", "정회운"],
+    },
+    {
+      key: 2,
+      title: "지수님과 약속",
+      peoples: "(2)",
+      value: ["장준수", "성지수"],
+    },
+    {
+      key: 3,
+      title: "프로젝트 약속",
+      peoples: "(5)",
+      value: ["장준수", "정회운", "성지수", "최강현", "황수경"],
+    },
   ]);
 
   const [listMode, setListMode] = useState(1);
   const list = [];
 
-  for(let i = 0; i < moyeoList.length; i++){
+  for (let i = 0; i < moyeoList.length; i++) {
     let data = moyeoList[i];
-    list.push(<li key={data.key} className='map-moyeo-list'>
-      <a id={'/moyeoList/'+data.key} className={'moyeo '+ (listMode === '/moyeoList/'+data.key && 'active')} href={'/entryList/'+data.key} onClick={event =>{
-        event.preventDefault();
-        setListMode(event.target.id);
-      }}>{data.title} {data.peoples}</a>
-    </li>)
+    list.push(
+      <li key={data.key} className="map-moyeo-list">
+        <a
+          id={"/moyeoList/" + data.key}
+          className={
+            "moyeo " + (listMode === "/moyeoList/" + data.key && "active")
+          }
+          href={"/entryList/" + data.key}
+          onClick={(event) => {
+            event.preventDefault();
+            setListMode(event.target.id);
+          }}
+        >
+          {data.title} {data.peoples}
+        </a>
+      </li>
+    );
   }
 
-  return <ul className='map-list-ul'>
-      {list}
-    </ul>
+  return <ul className="map-list-ul">{list}</ul>;
 }
 
 export default function Map() {
-
   useEffect(() => {
     KakaoMapCategory();
   }, []);
 
-  return <article className="article">
-    <div className="map-wrap">
-      <ul id="category">
-          <li id="CS2" data-order="0"> 
-              <span className="category-bg store"></span>
-              <span>편의점</span>
-          </li>       
-          <li id="SW8" data-order="1"> 
-              <span className="category-bg bank"></span>
-              <span>지하철</span>
-          </li>    
-          <li id="AT4" data-order="2"> 
-              <span className="category-bg bank"></span>
-              <span>관광</span>
-          </li>  
-          <li id="FD6" data-order="3"> 
-              <span className="category-bg bank"></span>
-              <span>음식점</span>
-          </li>  
-          <li id="CE7" data-order="4"> 
-              <span className="category-bg cafe"></span>
-              <span>카페</span>
-          </li>   
-          <li id="OL7" data-order="5"> 
-              <span className="category-bg oil"></span>
-              <span>주유소</span>
-          </li>      
-      </ul>
-    </div>
-    <div id="map"></div>
-    <div className="moyeo-list"><SelectList/></div>
-  </article>
+  return (
+    <article className="article">
+      <div className="map-wrap">
+        <ul id="category">
+          <li id="CS2" data-order="0">
+            <span className="category-bg store"></span>
+            <span>편의점</span>
+          </li>
+          <li id="SW8" data-order="1">
+            <span className="category-bg bank"></span>
+            <span>지하철</span>
+          </li>
+          <li id="AT4" data-order="2">
+            <span className="category-bg bank"></span>
+            <span>관광</span>
+          </li>
+          <li id="FD6" data-order="3">
+            <span className="category-bg bank"></span>
+            <span>음식점</span>
+          </li>
+          <li id="CE7" data-order="4">
+            <span className="category-bg cafe"></span>
+            <span>카페</span>
+          </li>
+          <li id="OL7" data-order="5">
+            <span className="category-bg oil"></span>
+            <span>주유소</span>
+          </li>
+        </ul>
+      </div>
+      <div id="map"></div>
+      <div className="moyeo-list">
+        <SelectList />
+      </div>
+    </article>
+  );
 }
